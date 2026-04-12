caf17f58b207476f25874274575fca9ec3c964ed
yarikmoscow <yarikbeatz@yandex.ru>
2026-04-12 05:26:52 +0300

    update game build

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d e:\github\loko-space
 copy /Y index_patched.html index.html
-powershell -Command "(Get-Content data.js -Raw) -replace '\"showWatermark\":true','\"showWatermark\":false' -replace '\"showGDevelopSplash\":true','\"showGDevelopSplash\":false' | Set-Content data.js -NoNewline"
+powershell -Command "(Get-Content data.js -Raw) -replace '\"showWatermark\":true','\"showWatermark\":false' -replace '\"showGDevelopSplash\":true','\"showGDevelopSplash\":false' -replace '\"adaptGameResolutionAtRuntime\":true','\"adaptGameResolutionAtRuntime\":false' -replace '\"sizeOnStartupMode\":\"scaleOuter\"','\"sizeOnStartupMode\":\"default\"' | Set-Content data.js -NoNewline"
 git add -A && git commit -m "update game build" && git push
 echo Done!
 pause
